a8e36898c024332e50baf15822aba4ab7e82adf0
Binod Gautam <kanika@bhangar.net>
2025-03-17 11:57:40 +0545

    feat: implement register endpoint

---
+++ register/register.js
@@ -0,0 +1,36 @@
+document.getElementById('registerForm').addEventListener('submit', async (e) => {
+    e.preventDefault()
+
+    const formData = {
+        first_name: document.getElementById('firstName').value,
+        last_name: document.getElementById('lastName').value,
+        email: document.getElementById('email').value,
+        phone: document.getElementById('phone').value,
+        dob: document.getElementById('dob').value,
+        gender: document.getElementById('gender').value,
+        address: document.getElementById('address').value,
+        password: document.getElementById('password').value,
+        role: 'super_admin'
+    };
+
+    try {
+        const response = await fetch('http://localhost:5500/auth/register', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(formData)
+        })
+        const data = await response.json();
+
+        if (response.ok) {
+            alert('Registration successful!');
+            window.location.href = '../index.html';
+        } else {
+            alert(data.message || 'Registration failed');
+        }
+    } catch (error) {
+        console.error('Error:', error);
+        alert('An error occurred during registration');
+    }
+})
